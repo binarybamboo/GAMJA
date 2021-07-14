21f7e7b4f1864419748a6228c13fa225b94b06d1
9mean <kanggm102@naver.com>
2021-07-14 14:50:51 +0900

    android/메인다이어리부분 더미데이터로 리사이클러뷰 구현과 아이템클릭리스너 달아서 각 다이어리의 일기들로 들어갈수있게 구현완료

--- android/gamja_android/app/src/main/java/com/example/gamja/view/MainActivity.kt
+++ android/gamja_android/app/src/main/java/com/example/gamja/view/MainActivity.kt
@@ -1,37 +1,50 @@
 package com.example.gamja.view
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.gamja.R
 import com.example.gamja.databinding.ActivityMainBinding
+import com.example.gamja.model.Diary
 import com.example.gamja.view.dialog.AddDiaryDialog
+import com.example.gamja.view.recyclerview.MainRecyclerViewAdapter
 import com.example.gamja.viewmodels.MainViewModel
 import com.kakao.sdk.user.UserApiClient
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
     private lateinit var userName:String
-    lateinit var mainViewModel: MainViewModel
+    private val mainViewModel: MainViewModel by lazy {
+        ViewModelProvider(this,MainViewModel.Factory(application)).get(MainViewModel::class.java)
+    }
+    lateinit var mainAdapter: MainRecyclerViewAdapter
+    val datas = arrayListOf<Diary>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        mainViewModel=ViewModelProvider(this).get(MainViewModel::class.java)
-        mainViewModel.userName.observe(this,{
-
-        })
-
         binding= DataBindingUtil.setContentView(this,R.layout.activity_main)
         binding.lifecycleOwner=this
         binding.vm=mainViewModel
         binding.activity = this@MainActivity
         val view = binding.root
         setContentView(view)
+        //리사이클러뷰 초기화
+        initRecycler()
+
+
+        mainViewModel.userName.observe(this,{
+
+        })
+        //데이터값이 변경되면 옵저빙으로 뷰렌더링
+        mainViewModel.diaryList.observe(this,{
+            mainAdapter.datas= it as ArrayList<Diary>
+            mainAdapter.notifyDataSetChanged()
+        })
 
         binding.logoutBtn.setOnClickListener {
             logOut()
@@ -64,6 +77,17 @@ class MainActivity : AppCompatActivity() {
             override fun onClicked() {
                 //다이어리추가
                 Log.d("TAG", "click dialog ")
+            }
+
+        })
+    }
+    private fun initRecycler() {
+        mainAdapter = MainRecyclerViewAdapter(this)
+        binding.diaryRecyclerView.adapter =mainAdapter
+        binding.diaryRecyclerView.layoutManager=GridLayoutManager(applicationContext,2)
+        mainAdapter.setItemClickListener(object : MainRecyclerViewAdapter.OnItemClickListener{
+            override fun onClick(v: View, position: Int) {
+                startActivity(Intent(applicationContext, SubDiaryActivity::class.java))
             }
 
         })
---
+++ android/gamja_android/app/src/main/java/com/example/gamja/view/recyclerview/MainRecyclerViewAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.gamja.view.recyclerview
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.gamja.R
+import com.example.gamja.model.Diary
+
+class MainRecyclerViewAdapter(private val context: Context): RecyclerView.Adapter<MainRecyclerViewAdapter.ViewHolder>() {
+    var datas = mutableListOf<Diary>()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.diary_list,parent,false)
+        return ViewHolder(view)
+    }
+    override fun getItemCount(): Int = datas.size
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(datas[position])
+        holder.diaryImg.setOnClickListener{
+            itemClickListener.onClick(it,position)
+        }
+    }
+    // (2) 리스너 인터페이스
+    interface OnItemClickListener {
+        fun onClick(v: View, position: Int)
+    }
+    // (3) 외부에서 클릭 시 이벤트 설정
+    fun setItemClickListener(onItemClickListener: OnItemClickListener) {
+        this.itemClickListener = onItemClickListener
+    }
+    // (4) setItemClickListener로 설정한 함수 실행
+    private lateinit var itemClickListener : OnItemClickListener
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val diaryTitle: TextView = itemView.findViewById(R.id.diary_item_title)
+        val diaryImg: ImageView = itemView.findViewById(R.id.diary_img)
+        fun bind(item: Diary) {
+            diaryTitle.text = item.title
+        }
+    }
+
+}
